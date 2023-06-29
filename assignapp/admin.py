812c5b6b76d3a1c86a65e1ea890588d782c6a69d
jojo <jyotm631@gmail.com>
2023-06-29 23:25:51 +0530

    Initial commit

--- assignapp/admin.py
+++ assignapp/admin.py
@@ -3,3 +3,9 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Product_color)
+admin.site.register(Apparel_size)
+admin.site.register(Product)
+admin.site.register(Product_Categories)
+admin.site.register(Categories)
+
+admin.site.register(Color)
--- assignapp/models.py
+++ assignapp/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Apparel_size(models.Model):
+    size_id=models.IntegerField()
+    size_code=models.CharField(max_length=120)
+    sort_order=models.CharField(max_length=120)
+    
+class Product(models.Model):
+    Apparel_sizes=models.ForeignKey(Apparel_size,on_delete=models.CASCADE) 
+    product_name=models.CharField(max_length=120)
+    product_id=models.IntegerField()
+    other_data=models.CharField(max_length=120)
+    
+class Product_Categories(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE) 
+    category_id=models.IntegerField()
+    
+class Categories(models.Model):
+    product_Categories=models.ForeignKey(Product_Categories,on_delete=models.CASCADE)
+    category_id=models.IntegerField()
+    parent_category_id=models.IntegerField()
+    category_name=models.CharField(max_length=120)
+
 class Product_color(models.Model):
     color_id=models.IntegerField()
+    color_name=models.CharField(max_length=130)
 
+class Color(models.Model):
+    Productcolour=models.ForeignKey(Product_color,on_delete=models.CASCADE)
+    colorid=models.IntegerField()
+    color_code=models.CharField(max_length=120)
+    color_name=models.CharField(max_length=120)
